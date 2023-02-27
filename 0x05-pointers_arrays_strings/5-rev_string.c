a87cf744279c06b9d7fbe3ee28694d3e7d549621
wasiu <adebayoabdulwasiu43@gmail.com>
2023-02-27 05:15:17 -0800

    my first commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  * rev_string - a function that reverses a string
+ *   * followed by a new line
+ *    * @s: an input string
+ *     * Return: Nothing
+ *      */
+void rev_string(char *s)
+{
+		int len = 0, i = 0;
+			char f;
+
+				while (s[len] != '\0')
+							len++;
+
+					while (i < len--)
+							{
+										f = s[i];
+												s[i++] = s[len];
+														s[len] = f;
+															}
+}
