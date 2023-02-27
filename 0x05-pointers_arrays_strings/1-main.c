21a71c857dfc39ee42cfd29d8cdfd160e413381c
wasiu <adebayoabdulwasiu43@gmail.com>
2023-02-27 12:38:57 -0800

    nistake

--- 0x05-pointers_arrays_strings/1-main.c
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *  * swap_int - swap the value of two integers.
+ *  * main - check the code
  *   *
- *    * @a: first integer.
- *     * @b: second integer.
- *      *
- */
-
-void swap_int(int *a, int *b)
+ *    * Return: Always 0.
+ *     */
+int main(void)
 {
-		int c;
+	    int a;
+	        int b;
 
-			c = 0;
-				c = *a;
-					*a = *b;
-						*b = c;
+			    a = 98;
+		        b = 42;
+			    printf("a=%d, b=%d\n", a, b);
+			        swap_int(&a, &b);
+				    printf("a=%d, b=%d\n", a, b);
+				        return (0);
 }
