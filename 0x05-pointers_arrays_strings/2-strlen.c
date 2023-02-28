748d1fdff45e731a0bb65cfd086f59557f2ab3a3
wasiu <adebayoabdulwasiu43@gmail.com>
2023-02-27 20:41:00 -0800

    last one

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,16 +5,16 @@
  *   *
  *    * @s: the string to calculate it's length.
  *     * Return: length of a string.
- */
+**/
 
 int _strlen(char *s)
 {
-		int i;
-			int length = 0;
+	int i;
+	int length = 0;
 
-			for (i = 0; s[i] != '\0'; i++)
-						{
-									length++;
-										}
-					return (length);
+		for (i = 0; s[i] != '\0'; i++)
+			{
+				length++;
+			}
+				return (length);
 }
