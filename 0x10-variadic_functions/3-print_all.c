11131f9a4af2f36849b12490ccc14e2569e65e25
wasiu <adebayoabdulwasiu43@gmail.com>
2023-03-24 00:37:41 -0700

    last one

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,56 @@
 #include "variadic_functions.h"
-/**
- * print_all - function that prints anything
- *
- * @format: is a list of types of arguments passed to the function
- */
-void print_all(const char * const format, ...)
-{
-	va_list list;
-	int x = 0;
-	char *k;
-	char *sep = ", ";
+	#include <stdarg.h>
+	#include <stdio.h>
 
-	va_start(list, format);
 
-	while ((format != NULL) && *(format + x) != '\0')
+	/**
+	 * print_all - prints anything
+	 * @format: list of types of arguments passed to the function
+	 */
+	void print_all(const char * const format, ...)
 	{
-		switch (*(format + x))
+		int i = 0;
+		char *str, *sep = "";
+
+
+		va_list list;
+
+
+		va_start(list, format);
+
+
+		if (format)
 		{
-			case 's':  /* string */
-				k = va_arg(list, char *);
-				k = (k != NULL) ? k : "(nil)";
-				printf("%s", k);
-				break;
-			case 'i':  /* int */
-				printf("%i", va_arg(list, int));
-				break;
-			case 'c':  /* char */
-				/* need a cast here since va_arg only takes fully promoted types */
-				printf("%c", va_arg(list, int));
-				break;
-			case 'f':  /* float */
-				printf("%f", va_arg(list, double));
-				break;
-			default:   /* if it does not comply with any of the cases */
-				x++;
-				continue;
+			while (format[i])
+			{
+				switch (format[i])
+				{
+					case 'c':
+						printf("%s%c", sep, va_arg(list, int));
+						break;
+					case 'i':
+						printf("%s%d", sep, va_arg(list, int));
+						break;
+					case 'f':
+						printf("%s%f", sep, va_arg(list, double));
+						break;
+					case 's':
+						str = va_arg(list, char *);
+						if (!str)
+							str = "(nil)";
+						printf("%s%s", sep, str);
+						break;
+					default:
+						i++;
+						continue;
+				}
+				sep = ", ";
+				i++;
+			}
 		}
-		if (*(format + x + 1) != 0)
-		{
-			printf("%s", sep);
-		}
-		x++;
+
+
+		printf("\n");
+		va_end(list);
 	}
-	putchar(10);
-	va_end(list);
-}
+
